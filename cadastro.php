52d0a04bb5eb9bd01140db61ecc2891aeeebdd96
Jefferson Ricardo <jeffersonricardoss10@gmail.com>
2026-03-10 08:44:10 -0300

    feat:Listar produtos

--- cadastro.php
+++ cadastro.php
@@ -20,7 +20,7 @@
             <ul class="list-group mb-3">
                 <li class="list-group-item d-flex justify-content-between lh-sm">
                     <div>
-                        <h6 class="my-0">Product name</h6> <small class="text-body-secondary">Brief description</small>
+                        <h6 class="my-0">Produto nome</h6> <small class="text-body-secondary">Brief description</small>
                     </div> <span class="text-body-secondary">$12</span>
                 </li>
                 <li class="list-group-item d-flex justify-content-between lh-sm">
