f5ab4e8d9e430ac7a7648cc95a3d4e6cda8bd162
rodrigodmd-pb <soaresrodrispb@gmail.com>
2026-03-10 09:16:08 -0300

    nova tabela

--- banco/script.sql
+++ banco/script.sql
@@ -6,3 +6,10 @@ CREATE TABLE clientes (
     senha VARCHAR(255),
 	data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE categorias(
+ 	id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR (80),
+ 	genero ENUM('masculino','feminino'),
+    descricao VARCHAR (300)
+);
