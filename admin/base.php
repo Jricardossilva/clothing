7cc070ff5c5b30e9c104f62140c23378739fe466
Diego Lucio <diegolucio780@gmail.com>
2026-03-24 11:40:51 -0300

    destruir sessão implementado

--- admin/base.php
+++ admin/base.php
@@ -1,4 +1,12 @@
 <?php
+
+session_start();
+
+if(!isset($_SESSION['usuario_id'])) {
+    header("Location: ../login.php");
+    exit;
+}
+
 // Defina a base do seu projeto (ajuste se necessário)
 define('BASE_URL', 'http://localhost/clothing/admin');
 ?>
