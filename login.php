5ff21d72c7990e459a5a1b58d9ea9090032ee1a1
John <johnherbertprog@gmail.com>
2026-03-24 12:03:04 -0300

    ajuste na sessão

--- login.php
+++ login.php
@@ -17,8 +17,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         
         if (password_verify($senha, $usuario['senha'])) {
             $_SESSION['usuario_id'] = $usuario['id'];
-            $_SESSION['nome'] = $usuario['nome'];            
-            header("Location: ./admin/");
+            $_SESSION['nome'] = $usuario['nome']; 
+            header("Location: admin/index.php");
             exit;
         } else {
             die("Senha ou Email não encontrado!");
