2ae3d77f77087a3eab0f64eed7e06a765deb852a
Diego Lucio <diegolucio780@gmail.com>
2026-04-23 11:56:01 -0300

    atualizacaodevteam

--- devs.php
+++ devs.php
@@ -4,22 +4,26 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dev Team</title>
-  <link rel="stylesheet" href="assets/css/curriculo.css">
 
-  <!-- Ícones -->
+  <link rel="stylesheet" href="assets/css/curriculo.css">
+  <link rel="stylesheet" href="assets/css/index.css" >
+
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+  <?php include 'includes/header.php'; ?>
 
-<div class="container">
+<div class="container-fluid">
+  <div class="grade">
 
-  <!-- CARD 1 -->
   <div class="flip-card">
     <div class="flip-card-inner">
       
       <div class="flip-card-front">
-        <img src="img_avatar.png">
-        <h2>Carlos Eduardo</h2>
+        <img src="../clothing/assets/img/carlos.jpg">
+        <h2>Carlos</h2>
       </div>
 
       <div class="flip-card-back">
@@ -29,8 +33,8 @@
           Desenvolveu a interface do site, organizando layout, cores e garantindo boa experiência do usuário.
         </p>
 
-        <div class="icons">
-          <a href="#"><i class="fab fa-github"></i></a>
+        <div class="icones">
+          <a href="https://github.com/Carlosmveloso" target="_blank"><i class="fab fa-github"></i></a>
           <a href="#"><i class="fab fa-linkedin"></i></a>
           <a href="#"><i class="fas fa-envelope"></i></a>
         </div>
@@ -39,12 +43,11 @@
     </div>
   </div>
 
-  <!-- CARD 2 -->
   <div class="flip-card">
     <div class="flip-card-inner">
       <div class="flip-card-front">
         <img src="../clothing/assets/img/diego2.jpg">
-        <h2>Diego Lucio</h2>
+        <h2>Diego</h2>
       </div>
 
       <div class="flip-card-back">
@@ -54,21 +57,20 @@
         Desenvolveu soluções integradas que abrangem desde a gestão de acesso e segurança dos usuários até a implementação de tecnologias interativas e a arquitetura visual da plataforma.
         </p>
 
-        <div class="icons">
-          <a href="https://github.com/DiegoLucioR"><i class="fab fa-github"></i></a>
-          <a href="#"><i class="fab fa-linkedin"></i></a>
+        <div class="icones">
+          <a href="https://github.com/DiegoLucioR" target="_blank"><i class="fab fa-github"></i></a>
+          <a href="https://www.linkedin.com/in/diegolucio780" target="_blank"><i class="fab fa-linkedin"></i></a>
           <a href="#"><i class="fas fa-envelope"></i></a>
         </div>
       </div>
     </div>
   </div>
 
-  <!-- CARD 3 -->
   <div class="flip-card">
     <div class="flip-card-inner">
       <div class="flip-card-front">
-        <img src="img_avatar.png">
-        <h2>Ernanda Rubinelle</h2>
+        <img src="../clothing/assets/img/ernanda.jpg">
+        <h2>Ernanda</h2>
       </div>
 
       <div class="flip-card-back">
@@ -78,8 +80,8 @@
           Criou o design visual e a experiência do usuário, focando na usabilidade e estética do projeto.
         </p>
 
-        <div class="icons">
-          <a href="https://github.com/rubinelle"><i class="fab fa-github"></i></a>
+        <div class="icones">
+          <a href="https://github.com/rubinelle" target="_blank"><i class="fab fa-github"></i></a>
           <a href="#"><i class="fab fa-linkedin"></i></a>
           <a href="#"><i class="fas fa-envelope"></i></a>
         </div>
@@ -87,12 +89,11 @@
     </div>
   </div>
 
-  <!-- CARD 4 -->
   <div class="flip-card">
     <div class="flip-card-inner">
       <div class="flip-card-front">
         <img src="../clothing/assets/img/john.jpg">
-        <h2>John Herbert</h2>
+        <h2>John</h2>
       </div>
 
       <div class="flip-card-back">
@@ -102,8 +103,8 @@
           Modelou e organizou o banco de dados, garantindo armazenamento eficiente e seguro das informações.
         </p>
 
-        <div class="icons">
-          <a href="https://github.com/johnherbert1977"><i class="fab fa-github"></i></a>
+        <div class="icones">
+          <a href="https://github.com/johnherbert1977" target="_blank"><i class="fab fa-github"></i></a>
           <a href="#"><i class="fab fa-linkedin"></i></a>
           <a href="#"><i class="fas fa-envelope"></i></a>
         </div>
@@ -111,11 +112,10 @@
     </div>
   </div>
 
-  <!-- CARD 5 -->
   <div class="flip-card">
     <div class="flip-card-inner">
       <div class="flip-card-front">
-        <img src="img_avatar.png">
+        <img src="../clothing/assets/img/miguel.jpg">
         <h2>Miguel</h2>
       </div>
 
@@ -123,19 +123,17 @@
         <h3>Contribuições</h3>
 
         <p class="descricao">
-          Realizou testes no sistema, identificando erros e garantindo o funcionamento correto das funcionalidades.
+        Criou funcionalidades para gestão de produtos e categorias, acompanhamento de pedidos, navegação entre páginas e melhorias no frete, com integração ao banco de dados.
         </p>
 
-        <div class="icons">
-          <a href="https://github.com/gueltech"><i class="fab fa-github"></i></a>
-          <a href="#"><i class="fab fa-linkedin"></i></a>
+        <div class="icones">
+          <a href="https://github.com/gueltech" target="_blank"><i class="fab fa-github"></i></a>
           <a href="#"><i class="fas fa-envelope"></i></a>
         </div>
       </div>
     </div>
   </div>
 
-  <!-- CARD 6 -->
   <div class="flip-card">
     <div class="flip-card-inner">
       <div class="flip-card-front">
@@ -147,19 +145,28 @@
         <h3>Contribuições</h3>
 
         <p class="descricao">
-          Produziu a documentação do projeto, explicando funcionamento, estrutura e uso do sistema.
+      Realizou a modelagem do banco de dados, desenvolveu integrações para controle de estoque no back-end, aprimorou a autenticação e implementou a gestão de páginas de produto.
         </p>
 
-        <div class="icons">
-          <a href="https://github.com/rodrigodmd-pb"><i class="fab fa-github"></i></a>
-          <a href="https://br.linkedin.com/in/rodrigo-soares-9309b4167"><i class="fab fa-linkedin"></i></a>
+        <div class="icones">
+          <a href="https://github.com/rodrigodmd-pb" target="_blank"><i class="fab fa-github"></i></a>
+          <a href="https://br.linkedin.com/in/rodrigo-soares-9309b4167" target="_blank"><i class="fab fa-linkedin"></i></a>
           <a href="#"><i class="fas fa-envelope"></i></a>
         </div>
       </div>
     </div>
   </div>
+  </div>
 
 </div>
+</div>
 
+  <?php include 'includes/footer.php'; ?>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+    crossorigin="anonymous"></script>
+  <script src="assets/js/index.js"></script>
 </body>
+
 </html>
